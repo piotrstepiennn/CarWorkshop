72d34fa621caa4477ab080139be91df422c7dff7
Admin <Admin@DESKTOP-KL7MLHH>
2023-10-23 15:39:28 +0200

    Add project files.

---
+++ CarWorkshop.Infrastructure/Persistence/CarWorkshopDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarWorkshop.Infrastructure.Persistence
+{
+    public class CarWorkshopDbContext : DbContext
+    {
+        public DbSet<Domain.Entities.CarWorkshop> CarWorkshops { get; set; }
+
+        public CarWorkshopDbContext(DbContextOptions<CarWorkshopDbContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Domain.Entities.CarWorkshop>()
+                .OwnsOne(c => c.ContactDetails);
+        }
+    }
+}
